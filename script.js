167a007f57f6f75b9b778bea0339b5d118cd172a
ThePredatorBG <meatyskeleton@abv.bg>
2022-08-19 21:49:30 +0300

    Implemented weather forecast page

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 	hamburger.classList.remove("active");
 	navMenu.classList.remove("active");
 }))
+
+var button = document.querySelector('.button')
+// var inputValue = document.querySelector('.inputValue')
+var cityName = document.querySelector('.cityName');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+button.addEventListener('click', function()
+{
+	// fetch('https://pro.openweathermap.org/data/2.5/forecast/hourly?zip=7000,bg&appid=5f7edfcf5d7bb904e231e538fb098ad4')
+	fetch('https://api.openweathermap.org/data/2.5/weather?q=Ruse&units=metric&appid=5f7edfcf5d7bb904e231e538fb098ad4')
+	.then(response => response.json())
+	.then(data =>
+		{
+			var cityNameValue = data['name'];
+			var tempValue = data['main']['temp'];
+			var descValue = data['weather'][0]['description'];
+
+			cityName.innerHTML = "City: "+cityNameValue;
+			desc.innerHTML = "Description: "+descValue;
+			temp.innerHTML = "Temperature (Celsius): "+tempValue;
+
+			console.log(data);
+		})
+
+	// .catch(err => alert("err!"))
+})
