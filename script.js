f15f4a85c4b44addcb0bfa8df107704d0bf74dc0
ThePredatorBG <meatyskeleton@abv.bg>
2022-08-26 21:13:08 +0300

    Implemented 5-day forecast. Solves #6

--- script.js
+++ script.js
@@ -11,25 +11,61 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 	navMenu.classList.remove("active");
 }))
 
+var inputField = document.querySelector('.inputField')
 var button = document.querySelector('.button')
-var cityName = document.querySelector('.cityName');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
 
 button.addEventListener('click', function()
 {
-	fetch('https://api.openweathermap.org/data/2.5/weather?q=Ruse&units=metric&appid=5f7edfcf5d7bb904e231e538fb098ad4')
+	document.querySelector(".hideShow").style.visibility="visible";
+
+	fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputField.value+'&units=metric&appid=5f7edfcf5d7bb904e231e538fb098ad4')
 	.then(response => response.json())
 	.then(data =>
 		{
-			var cityNameValue = data['name'];
-			var tempValue = data['main']['temp'];
-			var descValue = data['weather'][0]['description'];
-
-			cityName.innerHTML = "City: "+cityNameValue;
-			desc.innerHTML = "Description: "+descValue;
-			temp.innerHTML = "Temperature (Celsius): "+tempValue;
+			document.querySelector("#day0Min").innerHTML = "Min: " + data.main.temp_min + "°C";
+			document.querySelector("#day0Max").innerHTML = "Max: " + data.main.temp_max + "°C";
+			document.querySelector("#img0").src = "http://openweathermap.org/img/wn/" + data.weather[0].icon+"@2x.png";
 
 			console.log(data);
 		})
+
+	 fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputField.value+'&units=metric&appid=5f7edfcf5d7bb904e231e538fb098ad4')
+	 .then(response => response.json())
+	 .then(data =>
+	 	{
+				for(i=0;i<5;i++)
+				{
+					document.querySelector("#day" + (i+1) + "Min").innerHTML = "Min: " + data.list[i].main.temp_min + "°C";
+				}
+				for(i=0;i<5;i++)
+				{
+					document.querySelector("#day" + (i+1) + "Max").innerHTML = "Max: " + data.list[i].main.temp_max + "°C";
+				}
+				for(i=0;i<5;i++)
+				{
+					document.querySelector("#img" + (i+1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+"@2x.png";
+				}
+		 		console.log(data);
+	 	})
+
+		const d = new Date();
+		const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+		function CheckDay(day)
+		{
+			if (day +d.getDay() > 6)
+			{
+				return day +d.getDay()-7;
+			}
+			else
+			{
+				return day + d.getDay();
+			}
+		}
+
+		document.querySelector("#day0").innerHTML = weekday[CheckDay(0)];
+		for(i=0;i<5;i++)
+		{
+			document.querySelector("#day"+(i+1)).innerHTML = weekday[CheckDay(i)];
+		}
 })
